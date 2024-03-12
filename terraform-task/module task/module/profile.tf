6999d7c684672cb05225748d16d0a77136ba9a51
siddhant-ms07 <160129012+siddhant-ms07@users.noreply.github.com>
2024-03-12 17:26:43 +0530

    profile.tf

--- terraform-task/module task/module/profile.tf
+++ terraform-task/module task/module/profile.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  secret_key = "Jj9w8M9PHIRbzR0b50G2wGUPpCXmbXL/ZLgAbz94"
-  access_key = "AKIAYS2NRW4DWKMC5PGK"
+  secret_key = ""
+  access_key = ""
 }
 
 
